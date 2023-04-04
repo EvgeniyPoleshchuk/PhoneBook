fc8497b8b643f1d0fcc5dd2779c769866b93d112
Евгений Полещук <>
2023-04-04 11:26:03 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addGroup("Family");
+        phoneBook.addGroup("Job");
+        phoneBook.addGroup("Friends");
+
+        Contact contact = new Contact("Sasha", "8911");
+        Contact contact2 = new Contact("Misha", "12314");
+        Contact contact3 = new Contact("Phillip", "51231");
+
+        phoneBook.addContactInGroup(new String[]{"Family", "Friends"}, contact);
+        phoneBook.addContactInGroup(new String[]{"Job", "Friends"}, contact2);
+        phoneBook.addContactInGroup(new String[]{"Job", "Family"}, contact3);
+
+        phoneBook.printPhoneBook();
+
+        System.out.println("Поиск контакта : 1.По группе 2.По номеру телефона");
+        String input = scanner.nextLine();
+        int option = Integer.parseInt(input);
+        if (option == 1) {
+            String name = scanner.nextLine();
+            System.out.println(phoneBook.search(name));
+        }
+        if (option == 2) {
+            String number = scanner.nextLine();
+            System.out.println(phoneBook.searchPhoneNumber(number));
+            
+        }else{
+          System.out.println("Команда не найдена");
+        }
+    
+    }
+
+}
---
+++ PhoneBook.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class PhoneBook {
+    private final Map<String, List<Contact>> phoneBook = new HashMap<>();
+
+    public void addGroup(String name) {
+        phoneBook.put(name, new ArrayList<>());
+    }
+
+    public void addContactInGroup(String[] nameGroup, Contact contact) {
+        for (String group : nameGroup) {
+            if (!phoneBook.containsKey(group)) {
+                System.out.println(Arrays.toString(nameGroup) + " Такой группы нет");
+              break;
+            } else {
+                List<Contact> list = phoneBook.get(group);
+                list.add(contact);
+            }
+        }
+    }
+
+    public void printPhoneBook() {
+        for (Map.Entry<String, List<Contact>> entry : phoneBook.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public String search(String nameGroup) {
+        if (phoneBook.containsKey(nameGroup)) {
+            return phoneBook.get(nameGroup).toString();
+        } else {
+            return null;
+        }
+    }
+
+    public Contact searchPhoneNumber(String number) {
+        for (Map.Entry<String, List<Contact>> kv : phoneBook.entrySet()) {
+            for (Contact list : kv.getValue()) {
+                if (number.equals(list.getPhoneNumber())) {
+                    return list;
+                }
+            }
+        }
+        return null;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
